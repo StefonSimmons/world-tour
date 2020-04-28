0f3423725681292e2a337d3a336d7518f8075856
Stefon Simmons <stefonsimmons@Stefons-MacBook-Air.local>
2020-04-28 15:04:03 -0400

    installed styled components

--- world-tour/src/components/HomePage/Home.js
+++ world-tour/src/components/HomePage/Home.js
@@ -1,16 +1,27 @@
 import React from 'react'
+import '../HomePage/Home.css'
+
+
+
 
 function Home({totalCountries, totalArea, totalPopulation}) {
   return (
     <div>
-      <h1>Welcome, 3rd-Star Explorer</h1>
-      <h2>The population on Earth:</h2>
-      <p>{totalPopulation}</p>
-      <h2>Area of Land (mi<sup>2</sup>)</h2>
-      <p>{totalArea}</p>
-      <h2>Recorded Designated Regions:</h2>
-      <p>{totalCountries}</p>
 
+      <main>
+        <section>
+          <h1>Welcome, 3rd-Star Explorer</h1>
+
+          <h2>The population on Earth:</h2>
+          <p>{totalPopulation}</p>
+
+          <h2>Area of Land (mi<sup>2</sup>)</h2>
+          <p>{totalArea}</p>
+
+          <h2>Recorded Designated Regions:</h2>
+          <p>{totalCountries}</p>
+        </section>
+      </main>
     </div>
   )
 }
