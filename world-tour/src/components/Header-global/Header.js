afbf446bd54a7d8886d39372f52abcd3482e90ba
Stefon Simmons <stefonsimmons@Stefons-MacBook-Air.local>
2020-04-30 00:43:42 -0400

    converted media queries to styled components in Header

--- world-tour/src/components/Header-global/Header.js
+++ world-tour/src/components/Header-global/Header.js
@@ -2,7 +2,6 @@ import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
 import earth from '../../images/Emblem-earth.svg'
 import hamburger from '../../images/hamburger.png'
-import './Header.css'
 import styled, {keyframes} from 'styled-components'
 
 
@@ -20,6 +19,12 @@ const Wrapper = styled.ul`
 `
 const Hamburger= styled.img`
   display: none;
+
+  @media(max-width: 861px){
+    display: inline-block;
+    width: 40px;
+    padding: 6px 40px
+  }
 `
 const EarthSpin = keyframes`
   from {
@@ -38,6 +43,36 @@ const Earth = styled.img`
   box-shadow: 5px 10px 20px #F4D35E;
   cursor: pointer;
   animation: ${EarthSpin} infinite 50s linear;
+  
+  @media(max-width: 861px){   
+    right: 35vw;
+  }
+
+  @media(max-width: 800px){
+    right: 30vw;
+  }
+`
+const MenuItemContainer = styled.li`
+  display: inline-block;
+  list-style-type: none;
+  padding: 15px;
+  border-left: solid 1px grey;
+
+  @media(max-width: 861px){
+    display: none; 
+    background-color: #282c34;
+  }
+`
+const MenuLink = styled(Link)`
+  display: inline block;
+  color: white;
+  text-decoration: none;
+  text-align: center;
+  margin: 25px;
+
+  @media(max-width: 861px){   
+    margin: 25px 15px;
+  }
 `
 
 function Header() {
@@ -51,22 +86,26 @@ function Header() {
       <AppHeader>
         <NavBar>
           <Wrapper>
-            <Hamburger id="hamburger" src={hamburger} alt="hamburger menu" onClick={(e) => updateOpen(!open)}/>
-            <li className= "menu-item-container" style={open ? { display:"block" }: {display:""}}>
-              <Link to='/' className="menu-item" style={open ? { display:"block" }: {display:""}}>
+            <Hamburger src={hamburger} alt="hamburger menu" onClick={(e) => updateOpen(!open)}/>
+            
+            <MenuItemContainer style={open ? { display: "block" } : { display: "" }}>
+              <MenuLink to='/' style={open ? { display:"block" }: {display:""}}>
                   Home
-              </Link>
-            </li>
-            <li  className= "menu-item-container" style={open ? { display:"block" }: {display:""}}>
-              <Link to='/about' className="menu-item" style={open ? { display:"block" }: {display:""}}>
+              </MenuLink>
+            </MenuItemContainer>
+
+            <MenuItemContainer style={open ? { display:"block" }: {display:""}}>
+              <MenuLink to='/about' style={open ? { display:"block" }: {display:""}}>
                   About
-              </Link>
-            </li>
-            <li  className= "menu-item-container" style={open ? { display:"block" }: {display:""}}>
-              <Link to='/contact' className="menu-item" style={open ? { display:"block" }: {display:""}}> 
+              </MenuLink>
+            </MenuItemContainer>
+
+            <MenuItemContainer style={open ? { display:"block" }: {display:""}}>
+              <MenuLink to='/contact' style={open ? { display:"block" }: {display:""}}> 
                   Contact
-              </Link>
-            </li>
+              </MenuLink>
+            </MenuItemContainer>
+
           </Wrapper>
         </NavBar>
         <div>
